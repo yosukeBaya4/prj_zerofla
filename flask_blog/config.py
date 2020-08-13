6feaf72775bbeb1d7abbf19ccd6a8e071de3eadd
vagrant <vagrant@localhost.localdomain>
2020-08-13 05:21:08 -0400

    modified .gitignore and two more files

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,5 +1,8 @@
 DEBUG = True
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 SECRET_KEY = 'secret_key_987321'
 USERNAME = 'john'
 PASSWORD = 'due123'
