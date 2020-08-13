b01d2854b774f6b6c7eeb2aa348f114f0cf42f28
vagrant <vagrant@localhost.localdomain>
2020-08-13 04:05:59 -0400

    proceeded, till chap20

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,2 +1,6 @@
 DEBUG = True
 
+SECRET_KEY = 'secret_key_987321'
+USERNAME = 'john'
+PASSWORD = 'due123'
+
--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,7 +1,32 @@
 from flask_blog import app
-from flask import request, redirect, render_template, flash, session
+from flask import request, redirect, url_for, render_template, flash, session
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
     return render_template('entries/index.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            flash('Username is not correct')
+        elif request.form['password'] != app.config['PASSWORD']:
+            flash('Password is not correct')
+        else:
+            session['logged_in'] = True
+            flash('You successfully logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('logged out')
+    return redirect(url_for('show_entries'))
+
 
